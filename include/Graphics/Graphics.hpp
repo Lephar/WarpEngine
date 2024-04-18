97cf541d238b8b52879b9d0e0d521db61d21b677
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-04-18 16:49:04 +0300

    Add precompiled header support

--- include/Graphics/Graphics.hpp
+++ include/Graphics/Graphics.hpp
@@ -1,18 +1,10 @@
 #pragma once
 
-#define VULKAN_HPP_NO_SETTERS
-#define VULKAN_HPP_NO_SMART_HANDLE
-#define VULKAN_HPP_NO_CONSTRUCTORS
-#define VULKAN_HPP_NO_NODISCARD_WARNINGS
-#define VULKAN_HPP_NO_SPACESHIP_OPERATOR
-
-#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_vulkan.h>
-#include <vulkan/vulkan.hpp>
+#include "Main.hpp"
 
 class Renderer;
+class Graphics;
 class Memory;
 class Image;
 class Buffer;
+class Framebuffer;
