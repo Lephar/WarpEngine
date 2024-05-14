9e021138e7e5562051d15b4be6e566dcf661e228
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-05-14 23:24:15 +0300

    Add getExtensions function to system, without window creation

--- include/System/System.hpp
+++ include/System/System.hpp
@@ -8,6 +8,7 @@ namespace System {
 	void initialize();
 
 	PFN_vkGetInstanceProcAddr getLoader();
+	std::vector<const char *> getExtensions();
 
 	Window createWindow(std::string title, int width, int height);
 	void destroyWindow(Window window);
--- src/System/System.cpp
+++ src/System/System.cpp
@@ -1,17 +1,35 @@
 #include "System/System.hpp"
 #include "System/Window.hpp"
-#include <SDL_video.h>
 
 namespace System {
+	PFN_vkGetInstanceProcAddr loader;
+	std::vector<const char *> extensions;
+
 	void initialize() {
 		SDL_Init(SDL_INIT_EVERYTHING);
 		SDL_Vulkan_LoadLibrary(nullptr);
 		SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+
+		loader = reinterpret_cast<PFN_vkGetInstanceProcAddr>(SDL_Vulkan_GetVkGetInstanceProcAddr());
+
+		SDL_Window *window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_VULKAN | SDL_WINDOW_HIDDEN);
+
+		uint32_t extensionCount;
+        SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
+
+        extensions.resize(extensionCount);
+        SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions.data());
+
+		SDL_DestroyWindow(window);
 	}
 
 	PFN_vkGetInstanceProcAddr getLoader() {
-		return reinterpret_cast<PFN_vkGetInstanceProcAddr>(SDL_Vulkan_GetVkGetInstanceProcAddr());
+		return loader;
 	}
+
+	std::vector<const char *> getExtensions() {
+        return extensions;
+    }
 
 	Window createWindow(std::string title, int width, int height) {
 		Window window;
