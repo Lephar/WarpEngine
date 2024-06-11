c7630b96425d6548b1fba08537817575a522b950
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-06-11 08:17:37 +0300

    Create instance in graphics

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -2,9 +2,11 @@
 
 #include <vulkan/vulkan_raii.hpp>
 
+#include "Graphics/Instance.hpp"
 #include "Graphics/Device.hpp"
 
 namespace Graphics {
+    Instance *instance;
     std::vector<Device *> devices;
 
     Instance *initialize(const char *title, std::vector<const char *> layers, std::vector<const char *> extensions) {
@@ -13,12 +15,16 @@ namespace Graphics {
         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 #endif // NDEBUG
 
+        instance = new Instance(title, layers, extensions);
+/*
         vk::raii::PhysicalDevices physicalDevices{*instance};
 
         for(auto physicalDevice : physicalDevices) {
             auto device = new Device{physicalDevice};
             devices.push_back(device);
         }
+*/
+    return nullptr;
     }
 
     Device *getDevice(size_t index) {
