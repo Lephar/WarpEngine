fa37b375897bae34c1f91dcfaf02279bb15d5178
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-07-09 16:49:30 +0300

    Add test code for graphics destroy into main

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* args[])
     auto window = System::initialize("Zero", 800, 600);
     auto instance = Graphics::initialize("Zero", std::vector<const char *>{}, window->getExtensions());
 
+    Graphics::destroy();
 
     return EXIT_SUCCESS;
 }
