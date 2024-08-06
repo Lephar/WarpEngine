723b96cbfc92537aa26501377d63de0c64f512e2
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-08-06 03:09:34 +0300

    Update main for runtime test

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include "System/System.hpp"
 #include "System/Window.hpp"
 #include "Graphics/Graphics.hpp"
+#include "Graphics/Instance.hpp"
 #include "Graphics/Device.hpp"
 
 int main(int argc, char* args[])
@@ -10,8 +11,13 @@ int main(int argc, char* args[])
 
     auto window = System::initialize("Zero", 800, 600);
     auto instance = Graphics::initialize("Zero", std::vector<const char *>{}, window->getExtensions());
+    auto device = Graphics::getDefaultDevice();
+
+    auto surface = window->createSurface(**instance->getInstance());
+    device->registerSurface(vk::raii::SurfaceKHR{*instance->getInstance(), surface});
 
     Graphics::destroy();
+    System::quit();
 
     return EXIT_SUCCESS;
 }
