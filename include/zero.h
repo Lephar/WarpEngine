8efe92fb0a1c80590ed81e9c5bf9b8d7d27af72c
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-09-22 10:21:27 +0300

    Create the precompiled header

--- include/zero.h
+++ include/zero.h
@@ -3,4 +3,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_log.h>
 #include <SDL2/SDL_vulkan.h>
+#include <SDL2/SDL_stdinc.h>
 #include <vulkan/vulkan.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
