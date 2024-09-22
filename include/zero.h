2ebd202905b347ee596791be888f39ec5ce05a4b
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-09-22 13:42:19 +0300

    Add necessary headers

--- include/zero.h
+++ include/zero.h
@@ -6,7 +6,13 @@
 #include <SDL2/SDL_stdinc.h>
 #include <vulkan/vulkan.h>
 
+#include <time.h>
+#include <math.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <limits.h>
 #include <assert.h>
+#include <sys/mman.h>
