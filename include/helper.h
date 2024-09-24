150e11247f5d80715342e41c9ada91587ffb65a0
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-09-24 16:40:04 +0300

    Add cross-platform popcount method

--- include/helper.h
+++ include/helper.h
@@ -2,8 +2,11 @@
 
 #ifndef NDEBUG
 SDL_LogPriority convertLogSeverity(VkDebugUtilsMessageSeverityFlagBitsEXT severity);
+
 VKAPI_ATTR VkBool32 VKAPI_CALL messageCallback( VkDebugUtilsMessageSeverityFlagBitsEXT severity,
                                                 VkDebugUtilsMessageTypeFlagsEXT type,
                                                 const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
                                                 void* pUserData);
 #endif // NDEBUG
+
+uint32_t popcount(uint32_t value);
--- src/helper.c
+++ src/helper.c
@@ -26,3 +26,15 @@ VKAPI_ATTR VkBool32 VKAPI_CALL messageCallback( VkDebugUtilsMessageSeverityFlagB
     return VK_FALSE;
 }
 #endif // NDEBUG
+
+// Not the most optimal solution but gets the job done
+uint32_t popcount(uint32_t value) {
+    uint32_t count = 0;
+
+    while(value) {
+        count += value & 1;
+        value >>= 1;
+    }
+
+    return count;
+}
