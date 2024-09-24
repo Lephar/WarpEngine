abf74cbf5a7e0a9181c1028e0182f7baf683dba5
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-09-24 23:33:42 +0300

    Add the main function for testing

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include "window.h"
+#include "instance.h"
+#include "surface.h"
+#include "device.h"
+
+int main(int argc, char *argv[]) {
+    (void) argc;
+
+    initialize();
+    createWindow(argv[0], 800, 600);
+    createInstance();
+    createSurface();
+    selectPhysicalDevice();
+    createDevice();
+    
+    //draw(NULL);
+
+    destroySurface();
+    destroyInstance();
+    destroyWindow();
+    quit();
+}
