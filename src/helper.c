4abd1b691047ab1b5a7b9478d3c194ed92419b8b
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-09-26 14:57:59 +0300

    Switch validation layers logger from SDL to custom logger

--- src/helper.c
+++ src/helper.c
@@ -1,31 +1,17 @@
 #include "helper.h"
 
-#ifndef NDEBUG
-SDL_LogPriority convertLogSeverity(VkDebugUtilsMessageSeverityFlagBitsEXT severity) {
-    if(severity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
-        return SDL_LOG_PRIORITY_ERROR;
-    else if(severity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
-        return SDL_LOG_PRIORITY_WARN;
-    else if(severity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT)
-        return SDL_LOG_PRIORITY_INFO;
-    else if(severity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)
-        return SDL_LOG_PRIORITY_VERBOSE;
-    else
-        return SDL_LOG_PRIORITY_DEBUG;
-}
-
 VKAPI_ATTR VkBool32 VKAPI_CALL messageCallback( VkDebugUtilsMessageSeverityFlagBitsEXT severity,
                                                 VkDebugUtilsMessageTypeFlagsEXT type,
                                                 const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
                                                 void* pUserData) {
+    (void)severity;
     (void)type;
     (void)pUserData;
 
-    SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, convertLogSeverity(severity), "\tValidation Layers:\t%s\n", pCallbackData->pMessage);
+    debug("Validation Layers:\t%s", pCallbackData->pMessage);
 
     return VK_FALSE;
 }
-#endif // NDEBUG
 
 // Not the most optimal solution but gets the job done
 uint32_t popcount(uint32_t value) {
