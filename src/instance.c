5bfd1e48732adcc0eca827806e938c5d89e90c9e
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-09-27 11:42:15 +0300

    Format instance debug logs

--- src/instance.c
+++ src/instance.c
@@ -37,12 +37,12 @@ void createInstance() {
         .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
         .pNext = NULL,
         .flags = {},
-        .messageSeverity =  VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+        .messageSeverity =  VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT   |
                             VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-                            VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+                            VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT    |
                             VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT,
-        .messageType =  VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-                        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        .messageType =  VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT     |
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT  |
                         VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
         .pfnUserCallback = messageCallback,
         .pUserData = NULL
@@ -72,12 +72,12 @@ void createInstance() {
         .ppEnabledExtensionNames = extensionNames
     };
 
-    debug("Instance layers (count = %d)", layerCount);
+    debug("Instance layers (count = %d):", layerCount);
     for(uint32_t index = 0; index < layerCount; index++) {
         debug("\t%s", layerNames[index]);
     }
 
-    debug("Instance extensions (count = %d)", extensionCount);
+    debug("Instance extensions (count = %d):", extensionCount);
     for(uint32_t index = 0; index < extensionCount; index++) {
         debug("\t%s", extensionNames[index]);
     }
