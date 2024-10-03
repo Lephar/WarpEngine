8efae3b591e969bc27cf8733103e37a0967d276e
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-10-03 18:07:51 +0300

    Implement buffer creation

--- src/buffer.c
+++ src/buffer.c
@@ -1,3 +1,26 @@
 #include "buffer.h"
+#include "memory.h"
 
 extern VkDevice device;
+
+void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer *buffer, VkDeviceMemory *bufferMemory)
+{
+	VkBufferCreateInfo bufferInfo = {};
+	bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	bufferInfo.usage = usage;
+	bufferInfo.size = size;
+
+	vkCreateBuffer(device, &bufferInfo, NULL, buffer);
+
+	VkMemoryRequirements memoryRequirements;
+	vkGetBufferMemoryRequirements(device, *buffer, &memoryRequirements);
+
+	VkMemoryAllocateInfo allocateInfo = {};
+	allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocateInfo.allocationSize = memoryRequirements.size;
+	allocateInfo.memoryTypeIndex = chooseMemoryType(memoryRequirements.memoryTypeBits, properties);
+
+	vkAllocateMemory(device, &allocateInfo, NULL, bufferMemory);
+	vkBindBufferMemory(device, *buffer, *bufferMemory, 0);
+}
