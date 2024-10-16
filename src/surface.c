52c66d2235cd3db31d5b65067d268fbf793b8abd
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-10-16 19:21:58 +0300

    Generate surface details

--- src/surface.c
+++ src/surface.c
@@ -1,13 +1,29 @@
 #include "surface.h"
+
 #include "helper.h"
+#include "queue.h"
 
 extern SDL_bool windowCreated;
 SDL_bool surfaceCreated = SDL_FALSE;
 
 extern SDL_Window *window;
+extern VkExtent2D extent;
 
 extern VkInstance instance;
 VkSurfaceKHR surface;
+extern VkPhysicalDevice physicalDevice;
+extern Queue graphicsQueue;
+
+VkBool32 surfaceSupport;
+uint32_t surfaceFormatCount;
+VkSurfaceFormatKHR *surfaceFormats;
+uint32_t presentModeCount;
+VkPresentModeKHR *presentModes;
+VkSurfaceCapabilitiesKHR surfaceCapabilities;
+
+uint32_t imageCount;
+VkSurfaceFormatKHR surfaceFormat;
+VkPresentModeKHR presentMode;
 
 void createSurface() {
     assert(windowCreated && !surfaceCreated);
@@ -18,10 +34,29 @@ void createSurface() {
     debug("Surface created");
 }
 
+void generateSurfaceDetails() {
+    assert(surfaceCreated);
+    vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, graphicsQueue.queueFamilyIndex, surface, &surfaceSupport);
+    assert(surfaceSupport);
+
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &surfaceFormatCount, NULL);
+    surfaceFormats = malloc(surfaceFormatCount * sizeof(VkSurfaceFormatKHR));
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &surfaceFormatCount, surfaceFormats);
+
+    vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, NULL);
+    presentModes = malloc(presentModeCount * sizeof(VkPresentModeKHR));
+    vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, presentModes);
+
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfaceCapabilities);
+}
+
 void destroySurface() {
     assert(surfaceCreated);
 
     vkDestroySurfaceKHR(instance, surface, NULL);
+
+    free(presentModes);
+    free(surfaceFormats);
 
     surfaceCreated = SDL_FALSE;
     debug("Surface destroyed");
