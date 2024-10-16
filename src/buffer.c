1d0aba6564e27be3ccd35f3a4d3962d6cc62eef0
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-10-16 11:37:02 +0300

    Bring buffer functions in line with memory

--- src/buffer.c
+++ src/buffer.c
@@ -3,17 +3,10 @@
 
 extern VkDevice device;
 
-Buffer createBuffer(VkDeviceSize size, VkBufferUsageFlags usage)
+void createBuffer(Buffer *buffer, VkBufferUsageFlags usage, VkDeviceSize size)
 {
-    Buffer buffer = {
-        .bufferCreated = VK_FALSE,
-        .size = size,
-        .buffer = {},
-        .memoryRequirements = {},
-        .memoryBound = VK_FALSE,
-        .memoryOffset = 0,
-        .memory = NULL
-    };
+    buffer->usage = usage;
+    buffer->size = size;
 
     VkBufferCreateInfo bufferInfo = {
         .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
@@ -26,18 +19,29 @@ Buffer createBuffer(VkDeviceSize size, VkBufferUsageFlags usage)
         .pQueueFamilyIndices = NULL
     };
 
-    vkCreateBuffer(device, &bufferInfo, NULL, &buffer.buffer);
-    vkGetBufferMemoryRequirements(device, buffer.buffer, &buffer.memoryRequirements);
-
-    buffer.bufferCreated = VK_TRUE;
-
-    return buffer;
+    vkCreateBuffer(device, &bufferInfo, NULL, &buffer->buffer);
+    vkGetBufferMemoryRequirements(device, buffer->buffer, &buffer->memoryRequirements);
 }
 
 void bindBufferMemory(Buffer *buffer, Memory *memory) {
     buffer->memory = memory;
     buffer->memoryOffset = alignMemory(memory, buffer->memoryRequirements);
 
-    vkBindBufferMemory(device, buffer->buffer, memory->memory, buffer->memoryOffset);
-    buffer->memoryBound = VK_TRUE;
+    VkBindBufferMemoryInfo bindInfo = {
+        .sType = VK_STRUCTURE_TYPE_BIND_BUFFER_MEMORY_INFO,
+        .pNext = NULL,
+        .buffer = buffer->buffer,
+        .memory = memory->memory,
+        .memoryOffset = buffer->memoryOffset
+    };
+
+    vkBindBufferMemory2(device, 1, &bindInfo);
+}
+
+void destroyBuffer(Buffer *buffer) {
+    vkDestroyBuffer(device, buffer->buffer, NULL);
+
+    buffer->usage = 0;
+    buffer->size = 0;
+    buffer->memoryOffset = 0;
 }
