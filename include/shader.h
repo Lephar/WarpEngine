7365edd9561822a8c279bbe83551cca8dfd06343
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-10-29 19:36:00 +0300

    Implement shader module creation logic

--- include/shader.h
+++ include/shader.h
@@ -1,7 +1,13 @@
 #include "zero.h"
 
 struct shader {
-    const char *path;
+    const char *name;
     shaderc_shader_kind kind;
+    char *file;
+    size_t size;
+    uint32_t *code;
     VkShaderModule module;
 } typedef Shader;
+
+void createModules();
+void destroyModules();
--- src/shader.c
+++ src/shader.c
@@ -1,1 +1,66 @@
 #include "shader.h"
+
+#include "helper.h"
+
+extern char *path;
+extern VkDevice device;
+
+Shader vertex;
+Shader fragment;
+
+Shader createModule(const char *name, shaderc_shader_kind kind) {
+    Shader shader = {
+        .name = name,
+        .kind = kind,
+        .file = malloc(PATH_MAX)
+    };
+
+    debug("Shader: %s", shader.name);
+    debug("\tKind: %d", shader.kind);
+
+    const char *extension = "glsl";
+
+    if(shader.kind == shaderc_compute_shader) {
+        extension = "comp";
+    } else if(shader.kind == shaderc_vertex_shader) {
+        extension = "vert";
+    } else if(shader.kind == shaderc_fragment_shader) {
+        extension = "frag";
+    } //TODO: Add other shader types
+
+    sprintf(shader.file, "%s/shaders/%s.%s.spv", path, shader.name, extension);
+    debug("\tPath: %s", shader.file);
+
+    FILE *file = fopen(shader.file, "rb");
+    fseek(file, 0, SEEK_END);
+    shader.size = ftell(file);
+    rewind(file);
+
+    shader.code = malloc(shader.size);
+    fread(shader.code, 1, shader.size, file);
+    fclose(file);
+
+    debug("\tSize: %ld", shader.size);
+
+    VkShaderModuleCreateInfo shaderInfo = {
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .pNext = NULL,
+        .flags = 0,
+        .codeSize = shader.size,
+        .pCode = shader.code
+    };
+
+    vkCreateShaderModule(device, &shaderInfo, NULL, &shader.module);
+    debug("Successfully created");
+
+    return shader;
+}
+
+void createModules() {
+    vertex   = createModule("vertex"  , shaderc_vertex_shader  );
+    fragment = createModule("fragment", shaderc_fragment_shader);
+}
+
+void destroyModules() {
+
+}
