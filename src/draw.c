fd090689597b2794bdbcc666533f3ebe873c77e1
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-11-11 14:54:28 +0300

    Add necessary references to draw unit

--- src/draw.c
+++ src/draw.c
@@ -6,6 +6,30 @@
 #include "buffer.h"
 #include "element.h"
 
+extern VkExtent2D extent;
+
+extern Queue graphicsQueue;
+
+extern Swapchain swapchain;
+
+extern FramebufferSet framebufferSet;
+
+extern Buffer deviceBuffer;
+extern Buffer sharedBuffer;
+
+extern void *mappedSharedBuffer;
+
+extern uint32_t  indexCount;
+extern uint32_t vertexCount;
+
+extern VkDeviceSize   indexBufferSize;
+extern VkDeviceSize  vertexBufferSize;
+extern VkDeviceSize uniformBufferSize;
+
+extern Index    *  indexBuffer;
+extern Vertex   * vertexBuffer;
+extern Uniform  *uniformBuffer;
+
 uint32_t framebufferIndex;
 uint32_t swapchainImageIndex;
 uint32_t frameCount;
