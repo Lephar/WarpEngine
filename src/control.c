12181d75d248cca64cece3a0f5df1b76883500f3
Ali Emre Gülcü <aliemreglc@gmail.com>
2024-12-24 10:20:05 +0300

    Implement proper uniform buffer logic

--- src/control.c
+++ src/control.c
@@ -5,10 +5,16 @@
 
 #include "content.h"
 
+extern VkExtent2D extent;
+
 struct timespec timeCurrent;
 float timeDelta; // In microseconds
 
-extern VkExtent2D extent;
+float aspectRatio;
+float fieldOfView;
+
+float nearPlane;
+float  farPlane;
 
 vec2 mouseDelta;
 
@@ -20,12 +26,37 @@ vec3 upGlobal;
 vec2 movementInput;
 vec2 movement;
 
+mat4 viewMatrix;
+mat4 projMatrix;
+
+extern VkDeviceSize uniformBufferSize;
+extern Uniform *uniformBuffer;
+extern void *mappedSharedMemory;
+
 void resetControls() {
     clock_gettime(CLOCK_MONOTONIC, &timeCurrent);
 
     int32_t discard;
     SDL_GetRelativeMouseState(&discard, &discard);
     glm_vec2_zero(mouseDelta);
+
+    glm_mat4_zero(viewMatrix);
+    glm_mat4_zero(projMatrix);
+
+    aspectRatio = (float) extent.width / (float) extent.height;
+    fieldOfView = M_PI_2;
+
+    nearPlane =  0.1f;
+     farPlane = 10.0f;
+
+    glm_vec3_zero(position);
+    glm_vec3_zero(forward);
+    glm_vec3_zero(left);
+    glm_vec3_zero(upGlobal);
+
+    forward[2]  = 1.0f;
+    left[0]     = 1.0f;
+    upGlobal[1] = 1.0f;
 }
 
 void processControlEvents() {
@@ -45,7 +76,14 @@ void processControlEvents() {
 
     movementInput[0] = states[SDL_SCANCODE_A] - states[SDL_SCANCODE_D];
     movementInput[1] = states[SDL_SCANCODE_W] - states[SDL_SCANCODE_S];
-}
 
-void postprocessFrameControls() {
+    // TODO: Actually calculate the values
+
+    vec3 target;
+    glm_vec3_add(position, forward, target);
+    glm_lookat(position, target, upGlobal, viewMatrix);
+
+    glm_perspective(fieldOfView, aspectRatio, nearPlane, farPlane, projMatrix);
+
+    glm_mat4_mul(projMatrix, viewMatrix, uniformBuffer->transform);
 }
