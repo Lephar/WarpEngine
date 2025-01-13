4ec2f8f824966fef5628c4ca5ec4091babd87f35
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-01-13 11:29:24 +0300

    Align with glTF structure

--- include/content.h
+++ include/content.h
@@ -1,38 +1,56 @@
 #pragma once
 
 #include "pch.h"
+#include "image.h"
+
+// TODO: Use data oriented design for content instead of object oriented
 
 typedef uint32_t Index;
-
-struct material {
-
-} typedef Material;
 
 struct vertex {
     vec3 position;
     vec3 texcoord;
 } typedef Vertex;
 
-struct mesh {
+struct material {
+    Image texture;
+    Image normal;
+    Image tangent; // TODO: What is this?
+} typedef Material;
+
+struct primitive {
     Index *indices;
     Vertex *vertices;
-    mat4 transform;
-    Material normal;
-    Material texture;
+    Material material;
+} typedef Primitive;
+
+struct mesh {
+    cgltf_size primitiveCount;
+    Primitive *primitive;
 } typedef Mesh;
+
+struct node {
+    mat4 transform;
+    cgltf_size meshCount;
+    Mesh *meshes;
+} typedef Node;
+
+struct model {
+    cgltf_size nodeCount;
+    Node *nodes;
+} typedef Model;
+
+struct scene {
+    uint32_t binary;
+    char name[PATH_MAX];
+    char fullpath[PATH_MAX];
+} typedef Scene;
 
 struct uniform {
     mat4 view;
     mat4 proj;
     mat4 camera;
 } typedef Uniform;
-
-struct model {
-    uint32_t binary;
-    char name[PATH_MAX];
-    char fullpath[PATH_MAX];
-
-} typedef Model;
 
 void initializeAssets();
 void loadAssets();
