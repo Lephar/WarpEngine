fbfead68fc79c8b6aedf22811a9c907f96e80618
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-02-04 00:54:04 +0300

    Remove redundant shader read code

--- src/file.c
+++ src/file.c
@@ -26,37 +26,3 @@ Data readFile(const char *path, FileType type) {
 
     return data;
 }
-
-void readShaderFile(const char *fullPath, uint32_t binary, size_t *size, char **data) {
-    char kind[UINT8_MAX];
-    char type[UINT8_MAX];
-
-    fscanf(file, "%s%s%s", shader->file.name, kind, type);
-
-    const char *kindExtension = ".glsl";
-    const char *typeExtension = "";
-
-    if(strncmp(kind, "compute", UINT8_MAX) == 0) {
-        shader->stage = VK_SHADER_STAGE_COMPUTE_BIT;
-        kindExtension = ".comp";
-    } else if(strncmp(kind, "vertex", UINT8_MAX) == 0) {
-        shader->stage = VK_SHADER_STAGE_VERTEX_BIT;
-        kindExtension = ".vert";
-    } else if(strncmp(kind, "fragment", UINT8_MAX) == 0) {
-        shader->stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-        kindExtension = ".frag";
-    } //TODO: Add other shader types
-
-    if(strncmp(type, "intermediate", UINT8_MAX) == 0 || strncmp(type, "spirv", UINT8_MAX) == 0) {
-        shader->intermediate = VK_TRUE;
-        typeExtension = ".spv";
-    } else {
-        shader->intermediate = VK_FALSE;
-    }
-
-    sprintf(shader->file.fullpath, "%s/shaders/%s", rootPath, shader->file.name);
-
-    debug("Shader: %s", shader->file.name);
-    debug("\tKind: %s", kind);
-    debug("\tType: %s", type);
-}
