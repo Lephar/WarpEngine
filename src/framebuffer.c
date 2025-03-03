4bc79e5f845d11a922faa306b8d86ecc361f6e85
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-03-04 00:58:35 +0300

    Set framebuffer formats as suggested by best practices

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -77,8 +77,8 @@ void createFramebufferSet() {
 
     framebufferSet.sampleCount = VK_SAMPLE_COUNT_4_BIT;
 
-    framebufferSet.depthStencilFormat = VK_FORMAT_D32_SFLOAT_S8_UINT;
-    framebufferSet.colorFormat = VK_FORMAT_R16G16B16A16_SFLOAT;
+    framebufferSet.depthStencilFormat = VK_FORMAT_D24_UNORM_S8_UINT;
+    framebufferSet.colorFormat = VK_FORMAT_B8G8R8A8_SRGB;
 
     framebufferSet.framebuffers = malloc(framebufferSet.framebufferImageCount * sizeof(Framebuffer));
 
