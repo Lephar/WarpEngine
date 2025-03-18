547b86c666009b48ef258c1a997c7a748fabe984
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-03-18 16:42:32 +0300

    Refactor Window

--- include/window.h
+++ include/window.h
@@ -2,18 +2,12 @@
 
 #include "pch.h"
 
-extern SDL_bool windowCreated;
 extern SDL_Window *window;
 
 extern uint32_t frameIndex;
 
 extern SDL_bool quitEvent;
 extern SDL_bool resizeEvent;
-
-extern uint32_t requiredInstanceExtensionCount;
-extern const char **requiredInstanceExtensionNames;
-
-extern PFN_vkGetInstanceProcAddr getInstanceProcAddr;
 
 void initializeSystem();
 void createWindow();
--- src/window.c
+++ src/window.c
@@ -6,15 +6,7 @@
 #include "helper.h"
 #include "control.h"
 
-SDL_bool systemInitialized = SDL_FALSE;
-SDL_bool windowCreated = SDL_FALSE;
-
-PFN_vkGetInstanceProcAddr getInstanceProcAddr;
 SDL_Window *window = NULL;
-uint32_t requiredInstanceExtensionCount = 0;
-const char **requiredInstanceExtensionNames = NULL;
-
-SDL_TimerID timer = 0;
 
 uint32_t frameIndex = 0;
 uint32_t frameIndexCheckpoint = 0;
@@ -22,45 +14,9 @@ uint32_t frameIndexCheckpoint = 0;
 SDL_bool   quitEvent = SDL_FALSE;
 SDL_bool resizeEvent = SDL_FALSE;
 
-void initializeSystem() {
-    assert(!systemInitialized);
-
-    // NOTICE: RenderDoc doesn't support Wayland
-    SDL_SetHint(SDL_HINT_VIDEODRIVER, "wayland");
-    SDL_SetHint(SDL_HINT_AUDIODRIVER, "pipewire");
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-    debug("SDL Video Driver: %s", SDL_GetCurrentVideoDriver());
-    debug("SDL Audio Driver: %s", SDL_GetCurrentAudioDriver());
-
-    // NOTICE: May be needed if debug code is changed from printf to SDL_Log
-    //SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
-
-    SDL_Vulkan_LoadLibrary(NULL);
-    getInstanceProcAddr = SDL_Vulkan_GetVkGetInstanceProcAddr();
-
-    systemInitialized = SDL_TRUE;
-    debug("System initialized");
-}
-
-void createWindow() {
-    assert(systemInitialized && !windowCreated);
-
-    window = SDL_CreateWindow(executableName, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, extent.width, extent.height, SDL_WINDOW_VULKAN);
-
-    SDL_Vulkan_GetDrawableSize(window, (int32_t *)&extent.width, (int32_t *)&extent.height);
-    SDL_Vulkan_GetInstanceExtensions(window, &requiredInstanceExtensionCount, NULL);
-
-    requiredInstanceExtensionNames = malloc(requiredInstanceExtensionCount * sizeof(const char *));
-    SDL_Vulkan_GetInstanceExtensions(window, &requiredInstanceExtensionCount, requiredInstanceExtensionNames);
-
-    windowCreated = SDL_TRUE;
-    debug("Window created:");
-    debug("\tWidth:  %u", extent.width );
-    debug("\tHeight: %u", extent.height);
-}
-
 #if DEBUG
+SDL_TimerID timer = 0;
+
 uint32_t timerCallback(uint32_t interval, void *userData) {
     (void) userData;
 
@@ -76,14 +32,44 @@ uint32_t timerCallback(uint32_t interval, void *userData) {
 }
 #endif
 
+void initializeSystem() {
+    // TODO: Ubuntu has issues with Wayland and RenderDoc doesn't support it yet, wait for update
+    SDL_SetHint(SDL_HINT_VIDEODRIVER, "x11,wayland,windows");
+    SDL_SetHint(SDL_HINT_AUDIODRIVER, "pulseaudio,pipewire,directsound");
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    debug("System initialized:");
+    debug("\tVideo Driver: %s", SDL_GetCurrentVideoDriver());
+    debug("\tAudio Driver: %s", SDL_GetCurrentAudioDriver());
+}
+
+PFN_vkVoidFunction getSystemLoader() {
+    return SDL_Vulkan_GetVkGetInstanceProcAddr();
+}
+
+void createWindow() {
+    window = SDL_CreateWindow(executableName, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, extent.width, extent.height, SDL_WINDOW_VULKAN);
+
+    SDL_Vulkan_GetDrawableSize(window, (int32_t *) &extent.width, (int32_t *) &extent.height);
+
+    debug("Window created:");
+    debug("\tWidth:  %u", extent.width );
+    debug("\tHeight: %u", extent.height);
+}
+
+SDL_bool getWindowExtensions(uint32_t *extensionCount, const char **extensionNames) {
+    return SDL_Vulkan_GetInstanceExtensions(window, extensionCount, extensionNames);
+}
+
 void initializeMainLoop() {
     SDL_ShowCursor(SDL_DISABLE);
     SDL_SetRelativeMouseMode(SDL_TRUE);
     SDL_SetWindowResizable(window, SDL_TRUE);
+
 #if DEBUG
     timer = SDL_AddTimer(SEC_TO_MSEC, timerCallback, NULL);
-    assert(timer);
 #endif
+
     initializeControls();
     generatePerspective();
 
@@ -119,28 +105,13 @@ void finalizeMainLoop() {
 }
 
 void destroyWindow() {
-    assert(!surfaceCreated && windowCreated);
-
-    free(requiredInstanceExtensionNames);
-    requiredInstanceExtensionCount = 0;
-
     SDL_DestroyWindow(window);
-    window = NULL;
 
-    extent.height = 0;
-    extent.width = 0;
-
-    windowCreated = SDL_FALSE;
     debug("Window destroyed");
 }
 
 void quitSystem() {
-    assert(!windowCreated && systemInitialized);
-
-    getInstanceProcAddr = NULL;
-    SDL_Vulkan_UnloadLibrary();
     SDL_Quit();
 
-    systemInitialized = SDL_FALSE;
     debug("System quitted");
 }
