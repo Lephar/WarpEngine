68ffbc1e2c842274b3d7b85d89105ff135099dd3
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-03-23 07:14:27 +0300

    Prefer improved variants of physical device queries

--- src/physicalDevice.c
+++ src/physicalDevice.c
@@ -34,16 +34,57 @@ void selectPhysicalDevice() {
 
     physicalDevice = devices[selectedIndex];
 
-    vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
-    vkGetPhysicalDeviceFeatures(physicalDevice, &physicalDeviceFeatures);
+    free(devices);
 
-    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memoryProperties);
+    VkPhysicalDeviceProperties2 physicalDeviceProperties2 = {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2,
+        .pNext = NULL,
+        .properties = {}
+    };
 
-    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, NULL);
+    vkGetPhysicalDeviceProperties2(physicalDevice, &physicalDeviceProperties2);
+
+    physicalDeviceProperties = physicalDeviceProperties2.properties;
+
+    VkPhysicalDeviceFeatures2 physicalDeviceFeatures2 = {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2,
+        .pNext = NULL,
+        .features = {}
+    };
+
+    vkGetPhysicalDeviceFeatures2(physicalDevice, &physicalDeviceFeatures2);
+
+    physicalDeviceFeatures = physicalDeviceFeatures2.features;
+
+    VkPhysicalDeviceMemoryProperties2 memoryProperties2 = {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MEMORY_PROPERTIES_2,
+        .pNext = NULL,
+        .memoryProperties = {}
+    };
+
+    vkGetPhysicalDeviceMemoryProperties2(physicalDevice, &memoryProperties2);
+
+    memoryProperties = memoryProperties2.memoryProperties;
+
+    vkGetPhysicalDeviceQueueFamilyProperties2(physicalDevice, &queueFamilyCount, NULL);
+
+    VkQueueFamilyProperties2 *queueFamilyProperties2 = malloc(queueFamilyCount * sizeof(VkQueueFamilyProperties2));
+
+    for(uint32_t queueFamilyIndex = 0; queueFamilyIndex < queueFamilyCount; queueFamilyIndex++) {
+        queueFamilyProperties2[queueFamilyIndex].sType = VK_STRUCTURE_TYPE_QUEUE_FAMILY_PROPERTIES_2;
+        queueFamilyProperties2[queueFamilyIndex].pNext = NULL;
+        memset(&queueFamilyProperties2[queueFamilyIndex].queueFamilyProperties, 0, sizeof(VkQueueFamilyProperties));
+    }
+
+    vkGetPhysicalDeviceQueueFamilyProperties2(physicalDevice, &queueFamilyCount, queueFamilyProperties2);
+
     queueFamilyProperties = malloc(queueFamilyCount * sizeof(VkQueueFamilyProperties));
-    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilyProperties);
+
+    for(uint32_t queueFamilyIndex = 0; queueFamilyIndex < queueFamilyCount; queueFamilyIndex++) {
+        queueFamilyProperties[queueFamilyIndex] = queueFamilyProperties2[queueFamilyIndex].queueFamilyProperties;
+    }
 
     debug("Physical device selected: %s", physicalDeviceProperties.deviceName);
 
-    free(devices);
+    free(queueFamilyProperties2);
 }
