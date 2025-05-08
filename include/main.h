5132982fcee4330c37416dc328df74cb339bdeb6
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-05-08 19:58:34 +0300

    Clear main carry network logic to new unit

--- include/main.h
+++ include/main.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// WARN: DO NOT include this header anywhere except main.c
-
-#include <SDL3/SDL_main.h>
-
+#include "network.h"
 #include "engine.h"
+
+// WARN: DO NOT include this header anywhere except main.c
+#include <SDL3/SDL_main.h>
--- src/main.c
+++ src/main.c
@@ -1,36 +1,14 @@
 #include "main.h"
 
-#include "logger.h"
-
 int main(int argc, char *argv[]) {
-    int32_t server = socket(AF_INET, SOCK_STREAM, 0);
+    initNetwork();
+    initEngine(argc, argv);
 
-    struct sockaddr_in addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(5316),
-        .sin_addr = {
-            .s_addr = inet_addr("127.0.0.1")
-        },
-        .sin_zero = {}
-    };
+    loopNetwork();
+    loopEngine();
 
-    socklen_t addrlen = sizeof(addr);
+    quitNetwork();
+    quitEngine();
 
-    int32_t retval = connect(server, (struct sockaddr *) &addr, addrlen);
-    assert(retval == 0);
-
-    char *ip = inet_ntoa(addr.sin_addr);
-    uint16_t port = ntohs(addr.sin_port);
-
-    debug("IP:   %s", ip);
-    debug("Port: %d", port);
-
-    while(1);
-
-    /*
-    initialize(argc, argv);
-    loop();
-    quit();
-    */
     return EXIT_SUCCESS;
 }
