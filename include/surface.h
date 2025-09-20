40ace10ecb10f683234541896b786dc5801fabb6
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-09-20 11:50:52 +0300

    Implement surface extension and feature getters

--- include/surface.h
+++ include/surface.h
@@ -11,7 +11,10 @@ extern VkSurfaceCapabilitiesKHR surfaceCapabilities;
 
 extern VkExtent2D surfaceExtent;
 
+char const **getSurfaceInstanceExtensions(uint32_t *count);
 void createSurface();
 VkBool32 getSurfaceSupport(VkPhysicalDevice physicalDeviceCandidate);
-void generateSurfaceDetails();
+void setSurfaceDetails();
+char const **getSurfaceDeviceExtensions(uint32_t *count);
+void *getSurfaceDeviceFeatures();
 void destroySurface();
